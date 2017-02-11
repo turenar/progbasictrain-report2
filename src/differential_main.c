8c2bbc5b2959e39969bf16aa26ec5c004a17de57
Turenar <sora@turenar.xyz>
2017-02-11 17:30:51 +0900

    :+1: add differential binary

---
+++ src/differential_main.c
@@ -0,0 +1,42 @@
+#include "config.inc.h"
+
+#include <gmp.h>
+#include <stdio.h>
+#include "expr.h"
+#include "parser.h"
+#include "world.h"
+#include "fns/fns.h"
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <expression>\n", argv[0]);
+		return 1;
+	}
+	const char* expr = argv[1];
+//	printf("expression:\t%s\n", expr);
+
+	int exitcode;
+	mat_world_t* world = mat_world_new();
+	mat_fn_put_stdfunc(world);
+
+	mat_parser_t* parser = mat_parser_new(world, expr);
+	mat_expr_t* e = mat_parser_parse(parser);
+	if (!e) {
+		mat_parser_describe_error_position(parser);
+		exitcode = 1;
+		goto free_parser;
+	}
+
+	mat_expr_t* differential = mat_op_make_differential(e);
+	mat_op_show_expression(differential);
+	printf("\n");
+
+	exitcode = 0;
+
+	mat_expr_free(differential);
+	mat_expr_free(e);
+free_parser:
+	mat_parser_free(parser);
+	mat_world_free(world);
+	return exitcode;
+}
