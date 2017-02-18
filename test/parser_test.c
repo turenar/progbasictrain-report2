3a146e67ed3859b973c60636c6423c13b845e996
Soma Ueda <sora@turenar.xyz>
2017-02-18 11:16:38 +0900

    :white_check_mark: parser_test check +/-, FuncWithNumeric

--- test/parser_test.c
+++ test/parser_test.c
@@ -68,6 +68,9 @@ static void test_parser_illegal_syntax(CuTest* tc) {
 	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, "Test[Test]");
 	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, "Test[Test[1]");
 	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, "Test[,]");
+	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, "Test[+]");
+	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, "Test[+1]");
+	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, "Test[-]");
 	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, "4..2");
 	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, ".");
 	check_parse_failure(tc, MAT_PARSER_UNEXPECTED_TOKEN, "[");
@@ -78,6 +81,7 @@ static void test_parser_illegal_syntax(CuTest* tc) {
 static void test_parser_unknown_func(CuTest* tc) {
 	check_parse_failure(tc, MAT_UNKNOWN_FUNC, "Unknown[]");
 	check_parse_failure(tc, MAT_UNKNOWN_FUNC, "tesT[]");
+	check_parse_failure(tc, MAT_UNKNOWN_FUNC, "Unkn0wn[]");
 }
 
 static void test_parser_arg_count_mismatch(CuTest* tc) {
@@ -88,6 +92,7 @@ static void test_parser_arg_count_mismatch(CuTest* tc) {
 
 static void test_parser_check_value(CuTest* tc) {
 	check_value(tc, MAT_SUCCESS, 2, "Test[1]");
+	check_value(tc, MAT_SUCCESS, 0, "Test[-1]");
 	check_value(tc, MAT_HAVE_VARIABLE, 0, "Test[x]");
 	check_value(tc, MAT_HAVE_VARIABLE, 0, "Test[x,1]");
 	check_value(tc, MAT_SUCCESS, 3, "Test[Test[1]]");
