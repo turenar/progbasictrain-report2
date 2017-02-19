b3a67df98e92fec7ab02b24926bf2d698660fba5
Soma Ueda <sora@turenar.xyz>
2017-02-19 16:46:07 +0900

    :green_heart: workaround without gmp

--- test/differential_test.sh
+++ test/differential_test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 function check() {
-       local expected="$1"
-       local actual="$(_pass _call "$2")"
-       test $? -eq 0 || _die
-       _pass test "${expected}" = "${actual}"
+	   local expected="$1"
+	   local actual="$(_pass _call "$2")"
+	   test $? -eq 0 || _die
+	   test "${actual}" = "-0" && actual=0 # workaround without gmp
+	   _pass test "${expected}" = "${actual}"
 }
 
 EXE_NAME=differential
