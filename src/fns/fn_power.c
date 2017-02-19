702e177aef40ac3407b610c167669ac735b1ff76
Soma Ueda <sora@turenar.xyz>
2017-02-19 19:32:53 +0900

    :+1: Power propagate constant

--- src/fns/fn_power.c
+++ src/fns/fn_power.c
@@ -54,10 +54,34 @@ static mat_expr_t* simplify(mat_world_t* w, const mat_expr_t* expr) {
 	mat_expr_t* a = mat_op_simplify(w, args[0]);
 	mat_expr_t* b = mat_op_simplify(w, args[1]);
 	if (mat_expr_is_const(b)) {
-		if (mat_expr_is_const(a) && mpq_sgn(a->value.constant) == 0 && mpq_sgn(b->value.constant) != 0) {
+		if (mat_expr_is_const(a) && (mpq_sgn(a->value.constant) != 0 || mpq_sgn(b->value.constant) != 0)) {
+			mpfr_t afr, bfr, rfr;
+			mpf_t rf;
+			mpq_t rq;
+
+			mpfr_init(afr);
+			mpfr_init(bfr);
+			mpfr_init(rfr);
+			mpf_init(rf);
+			mpq_init(rq);
+
+			mpfr_set_q(afr, a->value.constant, MPFR_RNDN);
+			mpfr_set_q(bfr, b->value.constant, MPFR_RNDN);
+			mpfr_pow(rfr, afr, bfr, MPFR_RNDN);
+			mpfr_get_f(rf, rfr, MPFR_RNDN);
+			mpq_set_f(rq, rf);
+
+			mat_expr_t* expr = mat_expr_new_const(rq);
+
+			mpfr_clear(afr);
+			mpfr_clear(bfr);
+			mpfr_clear(rfr);
+			mpf_clear(rf);
+			mpq_clear(rq);
 			mat_expr_free(a);
 			mat_expr_free(b);
-			return mat_expr_new_const_int(0);
+
+			return expr;
 		} else if (mpq_sgn(b->value.constant) == 0) {
 			// 厳密にはa!=0である必要があるが、x^0 = 1としたい
 			mat_expr_free(a);
@@ -66,6 +90,16 @@ static mat_expr_t* simplify(mat_world_t* w, const mat_expr_t* expr) {
 		} else if (mpq_cmp_ui(b->value.constant, 1, 1) == 0) {
 			mat_expr_free(b);
 			return a;
+		} else if (mat_expr_is_function(a) && a->op_def == &mat_fn_power) {
+			// (x^2) ^3 -> x^6
+			mat_expr_t* ab = a->value.expr.args[1];
+			if (mat_expr_is_const(ab)) {
+				mat_expr_t* new_a = mat_expr_new_from(a->value.expr.args[0]);
+				mat_expr_t* new_b = mat_fn_common_times(b, mat_expr_new_from(ab));
+				mat_expr_free(a);
+				// b is moved in new_b assignment
+				return mat_expr_new_bi_args(&mat_fn_power, new_a, new_b);
+			}
 		}
 	}
 	return mat_expr_new_bi_args(&mat_fn_power, a, b);
